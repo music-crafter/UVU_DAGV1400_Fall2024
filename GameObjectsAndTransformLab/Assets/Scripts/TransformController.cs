6e264be0f13e377c87d335a326bf9e8f7ac5dc9b
Noah Ghent <noahghent1@gmail.com>
2024-09-06 13:27:48 -0600

    Added target field to TransformController script
    Script now only targets the object specified in the target field

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
+    
+    public GameObject target;
+    
     private void Update()
     {
         // Move the target GameObject up and down
         var x = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(0, x, 0);
-        transform.position = p;
+        target.transform.position = p;
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        target.transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
         // Scale the target GameObject
     }
 }
