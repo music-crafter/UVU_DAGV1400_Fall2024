2f77de2bcb40b6d620b9cca88558275f1a1c8f7c
Noah Ghent <noahghent1@gmail.com>
2024-09-07 21:07:45 -0600

    Tweaked TransformController Script
    Added additional target fields
    Added cos function to y translation
    Added scale function
    Tweaked time values

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class TransformController : MonoBehaviour
 {
     
-    public GameObject target;
+    public GameObject target1;
+    public GameObject target2;
+    public GameObject target3;
     
     private void Update()
     {
         // Move the target GameObject up and down
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
-        target.transform.position = p;
+        var x = Mathf.PingPong(Time.time, 4);
+        var y = Mathf.Cos(Time.time * 2);
+        var p = new Vector3(x, y, 0);
+        target1.transform.position = p;
         // Rotate the target GameObject
-        target.transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        target2.transform.Rotate(new Vector3(60, 30, 0) * Time.deltaTime);
         // Scale the target GameObject
+        target3.transform.localScale = new Vector3(x, y, 1);
     }
 }
