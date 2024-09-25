ed14253ce7f1d084da86ef090426a34c86262139
Noah Ghent <noahghent1@gmail.com>
2024-09-24 21:15:41 -0600

    Refined Inputs & Started Animations
    Updated Control script to include gravity
    Added jump *NOTE removed isGrounded condition for testing
    Created .anim files & animation controller
    WIP - Animation transitions currently not functional

--- AdventureGame/Assets/Scripts/SimpleCharacterController.cs
+++ AdventureGame/Assets/Scripts/SimpleCharacterController.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleCharacterController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 2f;
+    public float gravity = -9.81f;
+    
     private CharacterController controller;
+    private Vector3 velocity;
     private Transform thisTransform;
-    private Vector3 movementVector = Vector3.zero;
     
     private void Start()
     {
@@ -18,18 +19,42 @@ public class SimpleCharacterController : MonoBehaviour
     private void Update()
     {
         MoveCharacter();
+        ApplyGravity();
         KeepCharacterOnAxis();
     }
-
+    
     private void MoveCharacter()
     {
-        movementVector.x = Input.GetAxis("Horizontal");
-        movementVector *= (moveSpeed * Time.deltaTime);
-        controller.Move(movementVector);
+        // Horizontal
+        var moveInput = Input.GetAxis("Horizontal");
+        var move = new Vector3(moveInput, 0f, 0f) * (moveSpeed * Time.deltaTime);
+        controller.Move(move);
+        
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
     }
 
+    private void ApplyGravity()
+    {
+        // Apply gravity when not grounded
+        if (!controller.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0f; // Resets velocity when grounded
+        }
+        // Apply velocity to controller
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
     private void KeepCharacterOnAxis()
     {
+        // Maintain character's Z position
         var currentPosition = thisTransform.position;
         currentPosition.z = 0f;
         thisTransform.position = currentPosition;
